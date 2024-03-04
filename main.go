4e29bbab489357d537667904257e1002f4990dac
carloseduribeiro <carloseribeiro96@gmail.com>
2024-03-03 21:40:54 -0300

    Finishing

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -10,12 +14,27 @@ func main() {
 	server := &http.Server{Addr: ":3000"}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(4 * time.Second)
+		time.Sleep(15 * time.Second)
 		w.Write([]byte("Hello World!"))
 	})
 
-	fmt.Println("Server is running at http://localhost:3000")
-	if err := server.ListenAndServe(); err != nil {
-		panic(err)
+	go func() {
+		log.Println("Starting running server at http://localhost:3000")
+		if err := server.ListenAndServe(); err != nil && http.ErrServerClosed != err {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Could not gracefully shutdown server: %v", err)
 	}
+	log.Println("Server shutdown completed")
 }
